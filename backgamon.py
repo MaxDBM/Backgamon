46dbc3a40f65dd422daac484b8b06559b9addba4
MaxMccullough <mdm6ua@virginia.edu>
2023-12-19 15:22:37 -0800

    class setup

---
+++ backgamon.py
@@ -0,0 +1,19 @@
+from board import Board
+
+# import tkinter as tk
+
+# root = tk.Tk()
+
+# root.geometry("800x500")
+# root.title("Backgammon")
+
+# label = tk.Label(root, text="Backgammon", font=("Arial", 18))
+# label.pack(padx=20, pady=20)	# padding
+
+# textbox = tk.Text(root, font=("Arial", 12))
+# textbox.pack()
+
+# root.mainloop()
+
+myBoard = Board()
+myBoard.printBoard()
---
+++ board.py
@@ -0,0 +1,40 @@
+# Board Class
+# Stores the board state and provides methods to manipulate it
+class Board:
+	# class variables
+	# Store how many pieces are in each location
+	blackPieceLocations = [0 for _ in range(24)]
+	whitePieceLocations = [0 for _ in range(24)]
+
+	# How many pieces are in the jail
+	blackJail = 0
+	whiteJail = 0
+
+	# Dice number
+	dice = (1, 1)
+
+	# Store who's turn it is
+	isBlackTurn = False
+
+	# constructor
+	def __init__(self):
+		# Set up the board
+		self.whitePieceLocations[0] = 2
+		self.whitePieceLocations[11] = 5
+		self.whitePieceLocations[16] = 3
+		self.whitePieceLocations[18] = 5
+		self.blackPieceLocations[5] = 5
+		self.blackPieceLocations[7] = 3
+		self.blackPieceLocations[12] = 5
+		self.blackPieceLocations[23] = 2
+
+	# methods
+	# print the board
+	def printBoard(self):
+		print("Black: ", end="")
+		for i in range(24):
+			print(self.blackPieceLocations[i], end=" ")
+		print("\nWhite: ", end="")
+		for i in range(24):
+			print(self.whitePieceLocations[i], end=" ")
+		print("\n")
